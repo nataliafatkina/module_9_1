39195f7011fefcabfa8d5beb2f70966e2b18f1c1
nataliafatkina <fatkina.natali@gmail.com>
2024-11-02 11:02:18 +0300

    Add files via upload

---
+++ module_9_1.py
@@ -0,0 +1,11 @@
+# Функция, которая принимает список и несколько функций
+# и возвращает словарь с названием функции и результатом ее выполнения
+def apply_all_func(int_list, *functions):
+    results = {}
+    for function in functions:
+         results[function.__name__] = function(int_list)
+    return results
+
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
